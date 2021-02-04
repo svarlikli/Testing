fe84acb3013bbb4920c2e2750966731d82e0e7ee
svarlikli <suatvarlikli89@gmail.com>
2021-02-05 00:43:02 +0300

    FirstProject

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,11 @@
+package test.java;
+
+/**
+ * The class containing your tests for the {@link Demo} class.  Make sure you
+ * test all methods in this class (including both 
+ * {@link Demo#main(String[])} and 
+ * {@link Demo#isTriangle(double, double, double)}).
+ */
+public class DemoTest {
+
+}
